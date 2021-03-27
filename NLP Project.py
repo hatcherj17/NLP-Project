cdd7af489cf937986ea2fb145ce80516826f8663
Jacob <hatcherj17@students.ecu.edu>
2021-03-27 16:16:26 -0400

    Reads the csv and creates New csv
    Reads a csv and currently just outputs the same data into a new csv file

--- NLP Project.py
+++ NLP Project.py
@@ -1,11 +1,12 @@
 import os
-from csv import reader
+import csv
+from collections import defaultdict
 from tkinter import *
 from tkinter import filedialog
 
 #The select file filedialog screen
 def browseFiles():
-    fN = filedialog.askopenfilename(initialdir = "/",
+    fN = filedialog.askopenfilename(initialdir = "./",
                                           title = "Select a File",
                                           filetypes = (("All files",
                                                         "*.*"),
@@ -17,19 +18,50 @@ Tk().withdraw()
 
 #Returns the filename from the fileDialog window.
 filename = browseFiles()
+column = defaultdict(list)
 
+fields = ['Course Code', 'Section Code', 'Section Display Name', 'Est. Enroll.', 'ISBN', 'Title', 'Author', 'Publisher', 'Date', 'Edition', 'Required?', 'Supplier', 'Link', 'OK to Use Newer Edition?', 'OK to Use Older Edition?', 'Section Status', 'Submitted By']
+
+col2 = []
+f = open(filename)
+csv_f = csv.reader(f)
+
+for row in csv_f:
+  col2.append(row)
+
+with open('output.csv','w', newline="") as csvfile:
+  write = csv.writer(csvfile)
+  write.writerow(fields)
+  write.writerows(col2[1:])
+
+
+print(col2[1:2])
+''' EVERYTHING BELOW IS TEMP AND WILL BE REMOVED OR USED
 #Opens the CSV file and puts the data into list
 with open(filename,encoding="utf8") as data:
-   csv_reader = reader(data)
-   listRows = list(csv_reader)
+   csv_reader = csv.DictReader(data)
+   for row in csv_reader:
+      for (k,v) in row.items():
+        column[k].append(v)'''
 
-print ("Total Data: ", len(listRows))
-#print all rows of data
-#print (listRows)
+''' How to remove from dictonary
+>>> a_dict = {'one': 1, 'two': 2, 'thee': 3, 'four': 4}
+>>> new_dict = {}  # Create a new empty dictionary
+>>> for key, value in a_dict.items():
+...     # If value satisfies the condition, then store it in new_dict
+...     if value <= 2:
+...         new_dict[key] = value
+...
+>>> new_dict
+{'one': 1, 'two': 2}'''
 
-#Print out just one row of data
-for i in listRows[:10]:
-    print(" " .join(map(str, i)))
-    #print("\n") #<-- prints out each row on a single line
+'''dicts = column['ISBN'], column['Title']
 
-
+with open('output.csv', 'w') as csvfile:
+    fieldnames = ['ISBN', 'Title']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for e in dicts:
+      writer.writerows({'ISBN': e , 'Title': e})'''
+'''
+'''
