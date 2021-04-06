b55209e27d8f6f49c56b2f871487c54bfe02f6b9
Jacob <hatcherj17@students.ecu.edu>
2021-04-06 14:34:28 -0400

    Removes data from CSV based on remove.txt

--- NLP Project.py
+++ NLP Project.py
@@ -1,67 +1,98 @@
 import os
 import csv
+import re
 from collections import defaultdict
 from tkinter import *
 from tkinter import filedialog
 
 #The select file filedialog screen
-def browseFiles():
+def browseFiles(fileType):
     fN = filedialog.askopenfilename(initialdir = "./",
-                                          title = "Select a File",
+                                          title = f"Select a {fileType} File",
                                           filetypes = (("All files",
                                                         "*.*"),
                                                        ("Text Files",
                                                         "*.txt*")))
     return fN
+
+#need this but not priority right now
+'''def checkforDup(index)
+  seen = set(item_list)
+
+  if item not in seen:
+    seen.add(item)
+    item_list.append(item)
+'''
+
+#Gets all the indexs of the words that should be removed
+def findindexes(dataFile, whatToDelete):
+  index = []
+  i = 0
+  for word in dataFile:
+    i += 1
+    for item in word:
+      if re.search(f'(?:{whatToDelete})', item, re.IGNORECASE):
+        index.append(i)
+  return index
+
+#Prints out the what should be removeds text and how many
+def removeFrom(removeText, dataFile):
+  indexOf = findindexes(dataFile, removeText)
+  print(f'Total number of Books with "{removeText}" in the title: {str(len(indexOf))}')
+  return indexOf
+
+#Outputs to new CSV file that has the reduced data
+def makeReducedCSV(filename, dataFile, fields):
+  with open(filename+'REDUCED.csv','w', newline="") as csvfile:
+    write = csv.writer(csvfile)
+    write.writerow(fields)
+    write.writerows(dataFile[1:])
+    csvfile.close()
+
 #Hides the Tk UI so that its not shown when the the file browser opens to load the files
 Tk().withdraw()
 
 #Returns the filename from the fileDialog window.
-filename = browseFiles()
-column = defaultdict(list)
+filename = browseFiles('CSV')
 
-fields = ['Course Code', 'Section Code', 'Section Display Name', 'Est. Enroll.', 'ISBN', 'Title', 'Author', 'Publisher', 'Date', 'Edition', 'Required?', 'Supplier', 'Link', 'OK to Use Newer Edition?', 'OK to Use Older Edition?', 'Section Status', 'Submitted By']
-
-col2 = []
+#Gets the data out of the original CSV file
+dataFile = []
 f = open(filename)
 csv_f = csv.reader(f)
-
 for row in csv_f:
-  col2.append(row)
+  dataFile.append(row)
 
-with open('output.csv','w', newline="") as csvfile:
-  write = csv.writer(csvfile)
-  write.writerow(fields)
-  write.writerows(col2[1:])
+#Gets the text document that contains key words not to included
+indexestonotinclude = []
+textFile = browseFiles('TXT')
+textFileRead = open(textFile, 'r')
+Lines = textFileRead.readlines()
+Lines = [x.strip() for x in Lines]
+for line in Lines:
+  if not line.startswith("#"):
+    indexestonotinclude.extend(removeFrom(line, dataFile))
 
+#prints how many items shouldnt be included contains duplicates
+print(len(indexestonotinclude))
 
-print(col2[1:2])
-''' EVERYTHING BELOW IS TEMP AND WILL BE REMOVED OR USED
-#Opens the CSV file and puts the data into list
-with open(filename,encoding="utf8") as data:
-   csv_reader = csv.DictReader(data)
-   for row in csv_reader:
-      for (k,v) in row.items():
-        column[k].append(v)'''
+#Sorts the list of indexes that shouldnt be included
+indexestonotinclude.sort()
 
-''' How to remove from dictonary
->>> a_dict = {'one': 1, 'two': 2, 'thee': 3, 'four': 4}
->>> new_dict = {}  # Create a new empty dictionary
->>> for key, value in a_dict.items():
-...     # If value satisfies the condition, then store it in new_dict
-...     if value <= 2:
-...         new_dict[key] = value
-...
->>> new_dict
-{'one': 1, 'two': 2}'''
+#Removes duplicates
+finalIndexToRemove = [i for n, i in enumerate(indexestonotinclude,start=0) if i not in indexestonotinclude[:n]]
 
-'''dicts = column['ISBN'], column['Title']
+#DEBUG: prints out all the indexes that should be removed from the file
+print("Total amount to be removed without duplicates: " + str(len(finalIndexToRemove)))
+#print(finalIndexToRemove) #<----SHOULD BE COMMENTED OUT FOR DEBUG PURPOSES ONLY
 
-with open('output.csv', 'w') as csvfile:
-    fieldnames = ['ISBN', 'Title']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for e in dicts:
-      writer.writerows({'ISBN': e , 'Title': e})'''
-'''
-'''
+#Column headers should be changed to programatically at some point from the original file
+fields = ['Course Code', 'Section Code', 'Section Display Name', 'Est. Enroll.', 'ISBN', 'Title', 'Author', 'Publisher', 'Date', 'Edition', 'Required?', 'Supplier', 'Link', 'OK to Use Newer Edition?', 'OK to Use Older Edition?', 'Section Status', 'Submitted By']
+
+reducedData = [element for i, element in enumerate(dataFile, start=1) if i not in finalIndexToRemove]
+
+#Outputs to new CSV file named output
+makeReducedCSV(filename, reducedData, fields)
+
+#Closing all files
+textFileRead.close()
+f.close()
